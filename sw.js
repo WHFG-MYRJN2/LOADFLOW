ebf2940dc4370f7160f7862471d8fa2f47c17215
WHFG-MYRJN2 <kemalrizqy71@gmail.com>
2026-08-13 10:07:56 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worke — Antrian Gudang PWA
 // Ganti versi ini setiap ada update agar cache otomatis refresh
-const CACHE   = 'LOADFLOW-v1.2.7';
+const CACHE   = 'LOADFLOW-v1.2.8';
 const ASSETS  = [
   './',
   './index.html',
